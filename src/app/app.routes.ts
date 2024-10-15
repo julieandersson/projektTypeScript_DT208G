5809393773d649ad815a86f738374609b6b893df
julieandersson <juan2301@student.miun.se>
2024-10-15 13:55:04 +0200

    skapar component contact och notfound, adderar paths för dessa i routes.ts och felmeddelande i notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
 import { SchemeComponent } from './scheme/scheme.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'courses', component: CoursesComponent },
-    { path: 'scheme', component: SchemeComponent }
+    { path: 'scheme', component: SchemeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent }
+
 ];
