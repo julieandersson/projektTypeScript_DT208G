9e9b988a8dcc468d415408e0dbcfe250fc58d728
julieandersson <juan2301@student.miun.se>
2024-10-16 10:04:47 +0200

    enkel footer i footercomponent för att få ett enkelt helhetsintryck av webbplatsen innan fortsatt utveckling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
